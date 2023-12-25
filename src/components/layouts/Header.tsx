1cd1812903a7258641345ca75c512aad3676e256
KaisAbiyyi <kaisabiyyi2@gmail.com>
2023-12-25 21:24:14 +0700

    [App] components: header fix

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { Button, buttonVariants } from "../ui/button";
 import { NavLink, useLocation } from "react-router-dom";
 import WelcomeHeader from "../ui/WelcomeHeader";
 import { Input } from "../ui/input";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 interface HeaderProps {
 
@@ -12,7 +13,7 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = () => {
     const location = useLocation()
 
-    const isLogin: boolean = true
+    const isLogin: boolean = false
     return (
         <header className="bg-slate-200">
             <div className="container px-4 mt-4">
@@ -51,15 +52,36 @@ const Header: FC<HeaderProps> = () => {
                             }
                         } else if (location.pathname === "/profile") {
                             return <div className="flex justify-between">
-                                <div className="flex flex-col flex-grow">
-                                    <h1 className="p-6 text-4xl">#username</h1>
+                                <div className="flex flex-col flex-grow gap-8">
+                                    <div className="flex justify-between">
+                                        <div className="flex items-end">
+                                            <img src="" alt="profile photo" className="w-40 h-40 rounded-full bg-slate-300" />
+                                            <h1 className="px-4 text-4xl font-semibold">First Name Last Name</h1>
+                                        </div>
+                                        <div className="flex flex-col items-end gap-2 px-4">
+                                            <NavLink to={"/blog"} className="font-semibold hover:text-primary">
+                                                Open in spotify
+                                            </NavLink>
+                                            <span className="font-semibold">1 Followers</span>
+                                            <div className="flex items-center gap-4 p-2 rounded-lg bg-primary">
+                                                <Avatar>
+                                                    <AvatarImage src="https://github.com/shadcn.png" />
+                                                    <AvatarFallback>CN</AvatarFallback>
+                                                </Avatar>
+                                                <div className="flex flex-col">
+                                                    <CardTitle className="text-base text-primary-foreground">Song Title</CardTitle>
+                                                    <CardTitle className="text-base">Ed Sheeran</CardTitle>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div className="flex">
                                         <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Overview</NavLink>
-                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Songs</NavLink>
-                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Artist</NavLink>
-                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Albums</NavLink>
-                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Genres</NavLink>
-                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile">Labels</NavLink>
+                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile/songs">Songs</NavLink>
+                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile/artists">Artists</NavLink>
+                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile/albums">Albums</NavLink>
+                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile/genres">Genres</NavLink>
+                                        <NavLink className={buttonVariants({ variant: "link" })} to="/profile/labels">Labels</NavLink>
                                     </div>
                                 </div>
                             </div>
