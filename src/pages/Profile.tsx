1ea43ba9308f61f9d3f46ca263f1af60ea7ee569
Ramenaru <ramenarudev@gmail.com>
2024-01-11 00:14:20 +0700

    [App] route fixing

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,9 +2,37 @@ import TopSongsList from '@/components/profile/TopSongs';
 import TopArtistsList from '@/components/profile/TopArtists';
 import Taste from '@/components/profile/Taste';
 
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const ProfilePage: FC = () => {
+  const [token, setToken] = useState<string | null>(localStorage.getItem('spotifyAuthToken'));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    console.log('ProfilePage: URL hash:', hash);
+
+    const params = new URLSearchParams(hash.substring(1));
+    const urlToken = params.get('access_token');
+    console.log('ProfilePage: Extracted Token:', urlToken);
+
+    if (urlToken) {
+      localStorage.setItem('spotifyAuthToken', urlToken);
+      setToken(urlToken);
+      console.log('ProfilePage: save to local storage');
+      window.history.replaceState({}, document.title, "/profile");
+    }
+  }, [navigate]);
+
+  useEffect(() => {
+    if (!token) {
+      console.log('ProfilePage: token null');
+      navigate('/');
+    }
+  }, [token, navigate]);
+
+
   return (
     <div className="flex flex-col gap-16">
       <div className='flex gap-16'>
